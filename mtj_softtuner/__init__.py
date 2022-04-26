8dfb036cfb7877b98ea9bf4f15c586acdff36daf
vfbd <ve.forbryderne@gmail.com>
2022-04-25 20:04:00 -0400

    Use versioneer to auto-manage package version in PEP 440 style

--- mtj_softtuner/__init__.py
+++ mtj_softtuner/__init__.py
@@ -1,3 +1,7 @@
+from ._version import get_versions
 from .core import *
 from .exceptions import ConfigurationError
 from .trainers.basic import BasicTrainer
+
+__version__ = get_versions()["version"]
+del get_versions
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import os
+import versioneer
 
 path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 install_requires = []
@@ -9,7 +10,8 @@ if os.path.isfile(path):
 
 setup(
     name="mtj-softtuner",
-    version="1.0.6",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="VE FORBRYDERNE",
     author_email="ve.forbryderne@gmail.com",
     url="https://github.com/ve-forbryderne/mtj-softtuner",
@@ -19,13 +21,17 @@ setup(
     install_requires=install_requires,
     license="Apache",
     classifiers=[
+        "Private :: Do Not Upload",
         "License :: OSI Approved :: Apache Software License",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
     ],
 )
