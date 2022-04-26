ce8c16b1acc18c4d8c42a382d6b464a8e7c3637c
vfbd <ve.forbryderne@gmail.com>
2022-04-26 02:16:07 -0400

    Versioneer now works when downloading non-release ZIP from GitHub

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 cd "${0%/*}"
+Format=" "
+hash="$Format:%H$"
+if [[ $hash != "" && $hash != *" "* && ! -d ".git" ]]; then
+    git init
+    git config commit.gpgsign false
+    git remote add origin https://github.com/ve-forbryderne/mtj-softtuner
+    git fetch -qa
+    git checkout -qb _alpha
+    git add .
+    git commit -qm "alpha"
+    git checkout -q $hash
+    git merge --allow-unrelated-histories --squash -X theirs _alpha -m "omega" &> /dev/null
+    git branch -qD _alpha
+    git reset -q HEAD
+    git checkout -q -- mtj_softtuner/_version.py
+fi
 git submodule sync
 git submodule update --remote --init --recursive
 ${1:-python3} -m pip uninstall -y mtj-softtuner
