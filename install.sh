6ccd33d4cead6ed3682f7c8da0aebc9fed82cc0d
vfbd <ve.forbryderne@gmail.com>
2022-04-30 14:27:40 -0400

    Add explanation in install.sh, and unset `commit.gpgsign` at the end

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ cd "${0%/*}"
 Format=" "
 hash="$Format:%H$"
 if [[ $hash != "" && $hash != *" "* && ! -d ".git" ]]; then
+    # This executes only if you downloaded this repository using GitHub's API
+    # (e.g. by using the "Download ZIP" link on this repository's GitHub page
+    # or the source code downloads on this repository's GitHub releases page)
+    # rather than by cloning this repository using `git clone`.
+    # In that case, the downloaded source code won't be a git repository, so
+    # this will initialize a git repository based on the one on GitHub,
+    # checkout to the correct commit and then replay all your changes, if any,
+    # on top.
+    # We need to do this so that Versioneer can display the correct version
+    # string.
     git init
     git config commit.gpgsign false
     git remote add origin https://github.com/ve-forbryderne/mtj-softtuner
@@ -16,6 +26,7 @@ if [[ $hash != "" && $hash != *" "* && ! -d ".git" ]]; then
     git branch -qD _alpha
     git reset -q HEAD
     git checkout -q -- mtj_softtuner/_version.py
+    git config --unset commit.gpgsign
     p='s/^hash[[:blank:]]*=[[:blank:]]*".*$/hash="\$For'
     p+='mat:%H\$"/'
     sed -i $p install.sh
